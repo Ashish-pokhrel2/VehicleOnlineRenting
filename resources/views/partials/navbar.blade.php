8e9f0be1e969c38efce8487f246ea2b324b6bcb6
Ashish-pokhrel2 <aashish.pokhrel.12122@gmail.com>
2026-04-30 10:38:36 +0545

    profile page created

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -27,12 +27,12 @@
 
         <div class="nav-right desktop-actions">
             @auth
-                <div class="customer-pill">
+                <a href="{{ route('profile.edit') }}" class="customer-pill">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="18" height="18">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                     </svg>
                     <span>{{ $isAdmin ? 'Admin' : 'Customer' }}</span>
-                </div>
+                </a>
         
                 <form method="POST" action="{{ route('logout') }}" style="display:inline;">
                     @csrf
